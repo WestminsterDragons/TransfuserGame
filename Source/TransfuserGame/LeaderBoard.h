c8726b8f6d67d22b0d9355040947acd81304d850
Alberto Dal Corso <45342453+Alby1998@users.noreply.github.com>
2020-02-28 17:24:58 +0000

    UI / Checkpoint

---
+++ Source/TransfuserGame/LeaderBoard.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "LeaderBoard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRANSFUSERGAME_API ULeaderBoard : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
